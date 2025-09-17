cd5a871eb70fd53956cd49671f3e3b1862647872
Nikita Shulha <shulgamalthael@gmail.com>
2025-09-17 10:00:19 +0300

    init lesson 3

---
+++ Условные конструкции/Условные конструкции/Условные конструкции.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+
+	short number;
+
+	std::cin >> number;
+
+	if (number > 3)
+	{
+		std::cout << "Число " << number << " больше чем 3";
+	}
+}

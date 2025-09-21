46dc03a0b8516c90fc99c4946182ea31fcadad95
Nikita Shulha <nikita.alexandrovich.shulha@gmail.com>
2025-09-21 17:34:55 +0300

    init Lesson3

---
+++ установка visual studio/Lesson3/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+void main()
+{
+    setlocale(LC_ALL, "RU");
+
+    int num1 = 1;
+    short num2 = 1;
+    long num3 = 3;
+
+    std::cout << num1 << std::endl << num2 << std::endl << num3 << std::endl;
+}

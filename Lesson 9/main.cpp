2d976dcb8a3a30e86a137a9299919f98c29b3a29
Nikita Shulha <nikita.alexandrovich.shulha@gmail.com>
2025-10-06 14:08:22 +0300

    Init Lesson 9

--- Lesson 9/main.cpp
+++ Lesson 9/main.cpp
@@ -1,4 +1,13 @@
+#include <iostream>
+using namespace std;
+
 void main()
 {
-    
+    cout << "What is your name?" << endl;
+
+    string name;
+
+    cin >> name;
+
+    cout << "Hello " << name << "!" << endl;
 }
